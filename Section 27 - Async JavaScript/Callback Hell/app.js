00203d8a2e94aac02d99ea955c8f1d32abf10b1b
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-11-28 19:18:37 +0200

    uploadS Changes

--- Section 27 - Async JavaScript/Callback Hell/app.js
+++ Section 27 - Async JavaScript/Callback Hell/app.js
@@ -10,43 +10,43 @@
 
 //example of callback function that takes in the function argument to do next
 const delayedColorDisplay = (color, delay, doNext) => {
-    setTimeout(() => {
-        document.body.style.backgroundColor = color;
-        doNext && doNext();
-    }, delay);
+  setTimeout(() => {
+    document.body.style.backgroundColor = color;
+    doNext && doNext();
+  }, delay);
 };
 
 //nested callback
-delayedColorDisplay("red", 1000, () => {
-    delayedColorDisplay("orange", 1000, () => {
-        delayedColorDisplay("yellow", 1000, () => {
-            delayedColorDisplay("green", 1000, () => {
-                delayedColorDisplay("skyblue", 1000, () => {
-                    delayedColorDisplay("blue", 1000, () => {
-                        delayedColorDisplay("violet", 1000, () => {});
-                    });
-                });
-            });
+delayedColorDisplay('red', 1000, () => {
+  delayedColorDisplay('orange', 1000, () => {
+    delayedColorDisplay('yellow', 1000, () => {
+      delayedColorDisplay('green', 1000, () => {
+        delayedColorDisplay('skyblue', 1000, () => {
+          delayedColorDisplay('blue', 1000, () => {
+            delayedColorDisplay('violet', 1000, () => {});
+          });
         });
+      });
     });
+  });
 });
 
 //usual structure of callback functions goes like this
 
-searchMoviesAPI(
-    "amadeus",
-    () => {
-        saveToDatabase(
-            movies,
-            () => {
-                // success callback, if it works, run this
-            },
-            () => {
-                // failure callback, if it doesn't work, run this
-            }
-        );
-    },
-    () => {
-        // if the api to search movies is down, do this
-    }
-);
+// searchMoviesAPI(
+//   'amadeus',
+//   () => {
+//     saveToDatabase(
+//       movies,
+//       () => {
+//         // success callback, if it works, run this
+//       },
+//       () => {
+//         // failure callback, if it doesn't work, run this
+//       }
+//     );
+//   },
+//   () => {
+//     // if the api to search movies is down, do this
+//   }
+// );
--- Section 27 - Async JavaScript/CreatingPromises/app.js
+++ Section 27 - Async JavaScript/CreatingPromises/app.js
@@ -1,33 +1,35 @@
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve("Satisfied color change request");
-        }, delay);
-    });
+const fakeRequest = url => {
+  return new Promise((resolve, reject) => {
+    const rand = Math.random();
+    setTimeout(() => {
+      if (rand < 0.5) resolve('Your fake data Here!');
+      reject('REQUEST ERROR!');
+    }, 1000);
+  });
 };
 
-delayedColorChange("red", 1000)
-    .then(() => {
-        delayedColorChange("orange", 1000);
-        console.log("Done");
-    })
-    .then(() => {
-        delayedColorChange("yellow", 1000);
-        console.log("Done");
-    })
-    .then(() => {
-        delayedColorChange("green", 1000);
-        console.log("Done");
-    })
-    .then(() => {
-        delayedColorChange("skyblue", 1000);
-        console.log("Done");
-    })
-    .then(() => {
-        delayedColorChange("indigo", 1000);
-    })
-    .then(() => {
-        delayedColorChange("violet", 1000);
-        console.log("Done");
-    });
+fakeRequest('/DOGS/1')
+  .then(data => {
+    console.log('DONE WITH REQUEST!!');
+    console.log('data is: ', data);
+  })
+  .catch(err => {
+    console.log('OHH NOO', err);
+  });
+
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+delayedColorChange('red', 1000)
+  .then(() => delayedColorChange('orange', 1000))
+  .then(() => delayedColorChange('yellow', 1000))
+  .then(() => delayedColorChange('green', 1000))
+  .then(() => delayedColorChange('blue', 1000))
+  .then(() => delayedColorChange('indigo', 1000))
+  .then(() => delayedColorChange('violet', 1000));
